4f93a0d8b48bc79947a2c11f1a4126be2eb46d14
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2026-03-03 19:43:07 -0600

    adding second component and change prefix in angular.json

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductDetailsComponent } from "../product-details/product-details.component";
 
 @Component({
-  selector: 'app-catalog',
-  imports: [],
+  selector: 'bot-catalog',
+  imports: [ProductDetailsComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
 export class CatalogComponent {
-  
+
 }
