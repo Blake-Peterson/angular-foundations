64d77f09d1c510b089eff25c3fb49e8acc5fc5ff
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2026-03-04 05:49:35 -0600

    binding

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'bot-product-details',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
+  product: IProduct;
 
+  constructor() {
+    this.product = {
+      id: 2,
+      description:
+        'A friendly robot head with two eyes and a smile -- great for domestic use.',
+      name: 'Friendly Bot',
+      imageName: 'head-friendly.png',
+      category: 'Heads',
+      price: 945.0,
+      discount: 0.2,
+    };
+  }
+
+  getImageUrl(product: IProduct) {
+    return '/images/robot-parts/' + product.imageName;
+  }
+
+  addToCart(product: IProduct, event: MouseEvent) {
+    product.name += ' (Added to cart)';
+    console.log(event);
+  }
 }
