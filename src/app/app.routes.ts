f3c2264288f97b5e93ff9e83b81805c49cd792db
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2026-03-06 12:08:12 -0600

    routing in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/catalog', pathMatch: 'full' }, //empty path to tell where it needs to need to match path exactly (full)
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'cart', component: CartComponent },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CatalogComponent, CartComponent],
+  imports: [RouterOutlet, SiteHeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
