d005dde61fab1f7ddcbb332349f11e55ac1ca6a5
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2026-03-09 20:39:04 -0500

    last changes for signals

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable } from '@angular/core';
 import { IProduct } from './product.model';
-import allProducts from './products.json';
+import { httpResource, HttpResourceRef } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  getProducts(): IProduct[] {
-    return allProducts;
+  private resource: HttpResourceRef<IProduct[] | undefined> = httpResource(
+    () => '/api/products',
+  );
+
+  getProducts() {
+    return computed(() => this.resource.value() ?? []);
   }
 }
